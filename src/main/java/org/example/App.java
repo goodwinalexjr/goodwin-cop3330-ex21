3c4d675eded9ba0853177c463a1f701958c4472a
Alex Goodwin <goodwinalexjr@knights.ucf.edu>
2021-06-06 12:07:23 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner number = new Scanner(System.in);
+        System.out.print("Please enter the number of the month: ");
+        String month = "Invalid month";
+        int m = number.nextInt();
+
+        switch(m){
+
+            case 1:
+                month = "The name of the month is January.";
+                break;
+            case 2:
+                month = "The name of the month is Febuary.";
+                break;
+            case 3:
+                month = "The name of the month is March.";
+                break;
+            case 4:
+                month = "The name of the month is April.";
+                break;
+            case 5:
+                month = "The name of the month is May.";
+                break;
+            case 6:
+                month = "The name of the month is June.";
+                break;
+            case 7:
+                month = "The name of the month is July.";
+                break;
+            case 8:
+                month = "The name of the month is August.";
+                break;
+            case 9:
+                month = "The name of the month is September.";
+                break;
+            case 10:
+                month = "The name of the month is October.";
+                break;
+            case 11:
+                month = "The name of the month is November.";
+                break;
+            case 12:
+                month = "The name of the month is December.";
+                break;
+
+
+        }
+
+        System.out.println(month);
+    }
+}
